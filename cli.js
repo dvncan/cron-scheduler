6d45b927c163f4b1cfb3e9e7ba1bf19e362f3529
Dvncan <duncan@igliaison.com>
2019-01-22 12:02:50 -0500

    Playing with a cron scheduler

--- cli.js
+++ cli.js
@@ -1,5 +1,6 @@
 const cron = require('node-cron');
 const fs = require('fs');
+var data1;
 
 //Grab args
 const [,, ...args] = process.argv
@@ -7,7 +8,14 @@ const [,, ...args] = process.argv
 //Print
 console.log(`${args}`)
 
+var data1
 fs.readFile(`./${args}`, (err, data) => 
 {	if (err) throw err;
 	console.log(`${data}`);
+	data1 = data;
+	
 })
+
+cron.schedule('05 * * * * *', () => {
+  console.log('running a task every minute' + data1);
+});

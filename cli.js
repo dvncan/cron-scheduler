89defc45e09bbb09e255987aa458e5c1904e81c7
Dvncan <duncan@igliaison.com>
2019-01-27 16:41:07 -0500

    successful cron schedule from keyboard input. Next i need to read input from a file

--- cli.js
+++ cli.js
@@ -1,21 +1,36 @@
-const cron = require('node-cron');
 const fs = require('fs');
-var data1;
+const readline = require('readline');
+const Duration = require('duration');
+require('datejs');
 
 //Grab args
 const [,, ...args] = process.argv
 
-//Print
-console.log(`${args}`)
+//input 0, 0, X - every hour how many seconds.
 
-var data1
-fs.readFile(`./${args}`, (err, data) => 
+//Today local time
+var time = Date.today();
+
+//Today + time to task
+var time2 = Date.today().addSeconds(args[0]).addMinutes(args[1]).addHours(args[2]);
+
+//distant to first requested time
+var duration = new Duration(time, time2);
+
+//Display seconds till task run
+console.log("seconds: ", duration.seconds);
+
+/*fs.readFile(`./${args}`, (err, data) => 
 {	if (err) throw err;
 	console.log(`${data}`);
 	data1 = data;
 	
-})
+	const rl = readline.createInterface({
+		input : fs.createReadStream(""text.txt)
+	});
 
-cron.schedule('05 * * * * *', () => {
-  console.log('running a task every minute' + data1);
-});
+	rl.on('line', (line) => {
+		console.log('Line from file:', line);
+	});
+	
+})*/

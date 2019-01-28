a9a187e77c26f010ef692708de598ddf25a8c50a
Dvncan <duncan@igliaison.com>
2019-01-27 21:45:03 -0500

    submission

--- README.md
+++ README.md
@@ -1,2 +1,11 @@
 # cron-scheduler
 mylo test question 2
+
+Input from CLI, I could not finish the read from text file. 
+
+My program takes the input arguments and organizes them into s, m & hrs. First I get local computer time, (switch to EST Tz) then add the wild card inputs. Time2 is 0:0:0 plus the time until the scheduled task. 
+
+Then I take the duration (difference) between the two times and output them in seconds.
+
+Complexity for time is N. With implementing the read file functionality it would be n^2 if I looped through to read everytime I called funtion, or log(n) if I grabbed all inputs on first loop.
+complexity for space is log(n). It is not linear, however with more inputs would not grow exponentially. 
--- cli.js
+++ cli.js
@@ -2,6 +2,33 @@ const fs = require('fs');
 const readline = require('readline');
 const Duration = require('duration');
 require('datejs');
+const clock = require('world-clock')();
+
+
+/*function t(time, s, m, h){
+	//Today + time to task
+	var time2 = Date.today().addSeconds(s).addMinutes(m).addHours(h-5);
+
+	if(time2 < time){
+		time.addHours(0);
+		time2.addHours(12);
+	}
+
+	console.log(time2);
+
+	//distant to first requested time
+	var duration = new Duration(time, time2);
+
+	//Display seconds till task run
+	console.log("seconds: ", duration.seconds);
+
+
+	console.log("seconds: ", duration.seconds/3600);
+	console.log("m: ", duration.minutes/60);
+	console.log("h: ", duration.hours);
+}*/
+
+//For some reason my computer thinks I am in GMT timezone
 
 //Grab args
 const [,, ...args] = process.argv
@@ -9,28 +36,67 @@ const [,, ...args] = process.argv
 //input 0, 0, X - every hour how many seconds.
 
 //Today local time
-var time = Date.today();
 
-//Today + time to task
-var time2 = Date.today().addSeconds(args[0]).addMinutes(args[1]).addHours(args[2]);
+console.log(clock.localDateTime('SYSTEM').toString());
 
-//distant to first requested time
-var duration = new Duration(time, time2);
+//reset for EST
+var time = new Date().addHours(-5);
 
-//Display seconds till task run
-console.log("seconds: ", duration.seconds);
+console.log(time);
 
-/*fs.readFile(`./${args}`, (err, data) => 
-{	if (err) throw err;
-	console.log(`${data}`);
-	data1 = data;
-	
-	const rl = readline.createInterface({
-		input : fs.createReadStream(""text.txt)
-	});
+var s = args[0], m = args[1], h = args[2], ar = "";
 
-	rl.on('line', (line) => {
-		console.log('Line from file:', line);
-	});
-	
-})*/
+//read file
+
+/*
+const contents = fs.readFileSync(`./${args}`, 'utf8');
+for(let j = 0; j < contents.length; j++){
+
+
+	if(contents.substr(j, 1) != '\n'){
+
+		
+		if(contents.substr(j, 1) === 'X'){
+			ar+=0;
+		}else if(contents.substr(j, 1) != ','){
+			ar+=contents.substr(j,1);
+		}else if(contents.substr(j,1) == ','){
+			ar+= ""
+		}
+		t(time, ar[0], ar[1] + ar[2], ar[3]);
+		
+	}
+}
+
+console.log(ar);*/
+
+//Select wildcard
+
+if(s === 'X'){
+	s = time.getSeconds();
+}
+
+if(m === 'X'){
+	m = time.getMinutes();
+}
+
+if(h === 'X'){
+	h = time.getHours() -6 ;
+}
+
+
+var time2 = Date.today().addSeconds(s).addMinutes(m).addHours(h-5);
+
+//switch 
+	if(time2 < time){
+		time.addHours(0);
+		time2.addHours(12);
+	}
+
+	console.log(time2);
+
+	//distant to first requested time
+	var duration = new Duration(time, time2);
+
+	//Display seconds till task run
+	console.log("seconds till task: ", duration.seconds);
